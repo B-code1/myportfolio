28fa21ab0078e2c427c1f60f24bf7ac63f9fbb00
BELINDA BIWOTT <jepkemoi75@gmail.com>
2024-11-21 23:39:40 +0300

    first commit

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function openMenu() {
 function closeMenu() {
   sideMenu.style.transform = 'translateX(16rem)';
 }
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 0) {
+    sideMenu.style.transform = 'translateX(16rem)';
+  }
+  if (window.scrollY === 0) {
+    sideMenu.style.transform = 'translateX(16rem)';
+  }
+  
+
